0de8363fd50873c7847d007c6fd8120373a7fce7
Hans Tschan <Hans.Tschan@gmail.com>
2017-08-08 21:03:04 +0200

    deployment fixed

--- TpIonic/gulpfile.js
+++ TpIonic/gulpfile.js
@@ -5,22 +5,7 @@ const gulp = require("gulp"),
   rename = require('gulp-rename'),
   shell = require('gulp-shell'),
   gutil = require('gulp-util'),
-  ftp = require('vinyl-ftp'),
-  runSequence = require('run-sequence'),
-  GulpSSH = require("gulp-ssh");
-
-var config = {
-  host: 'timepuncher.ch',
-  port: 22,
-  username: 'tpdotnetcore',
-  remotePath: '/sites/TpIonic/',
-  privateKey: fs.readFileSync('../../../ssh.tpdeploy.private.ppk')
-}
-
-var gulpSSHTimepuncher = new GulpSSH({
-  ignoreErrors: false,
-  sshConfig: config
-});
+  runSequence = require('run-sequence');
 
 gulp.task('default', ['copyConfig']);
 
@@ -28,7 +13,10 @@ gulp.task('default', ['copyConfig']);
  * Copy development config into src directory.
  */
 gulp.task('copyConfig', () => {
-  gulp.src('../../../timepuncher-client-config.ts')
+  var configFile = '../../../timepuncher-client-config.ts';
+  if (fs.exists('c:/sites/configdata/timepuncher-client-config.ts'))
+    configFile = 'c:/timepuncher-client-config.ts';
+  gulp.src(configFile)
     .pipe(gulp.dest('src'));
 });
 
@@ -36,7 +24,10 @@ gulp.task('copyConfig', () => {
  * Copy production config into src directory.
  */
 gulp.task('copyConfigProd', () => {
-  gulp.src('../../../timepuncher-client-config-timepuncher.ch.ts')
+  var configFile = '../../../timepuncher-client-config-timepuncher.ch.ts';
+  if (fs.exists('c:/sites/configdata/timepuncher-client-config-timepuncher.ch.ts'))
+    configFile = 'c:/sites/configdata/timepuncher-client-config-timepuncher.ch.ts';
+  gulp.src(configFile)
     .pipe(rename('timepuncher-client-config.ts'))
     .pipe(gulp.dest('src'));
 });
@@ -45,7 +36,7 @@ gulp.task('copyConfigProd', () => {
  * Remove build directory.
  */
 gulp.task('clean', (cb) => {
-  return del(["www","platforms/browser"], cb);
+  return del(["www", "platforms/browser"], cb);
 });
 
 /**
@@ -57,23 +48,15 @@ gulp.task('buildbrowser', shell.task(['ionic cordova build browser']));
  * Upload distribution to timepuncher.ch.
  */
 gulp.task('upload', function () {
-  var conn = ftp.create({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    parallel: 10,
-    log: gutil.log,
-    secure: true
-  });
 
   var globs = ['platforms/browser/www/**'];
 
   // using base = '.' will transfer everything to /public_html correctly
   // turn off buffering in gulp.src for best performance
 
-  return gulp.src(globs, { base: 'platforms/browser/www', buffer: false })
-    .pipe(conn.newer('/')) // only upload newer files
-    .pipe(conn.dest('/'));
+  // return gulp.src(globs, { base: 'platforms/browser/www', buffer: false })
+  //   .pipe(conn.newer('/')) // only upload newer files
+  //   .pipe(conn.dest('/'));
 });
 
 /**
