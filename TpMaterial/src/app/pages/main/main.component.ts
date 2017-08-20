b292d7aeca30512d2a2b17085dcc9a93ae64b799
Hans Tschan <Hans.Tschan@gmail.com>
2017-08-20 17:22:15 +0200

    fixed dialog

--- TpMaterial/src/app/pages/main/main.component.ts
+++ TpMaterial/src/app/pages/main/main.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MdIconRegistry, MdDialog } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
-
-import { DialogComponent } from '../../dialog/dialog.component';
 
 @Component({
   selector: 'app-main',
@@ -10,34 +6,9 @@ import { DialogComponent } from '../../dialog/dialog.component';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
   isDarkThem = false;
 
-  isDarkTheme = false;
-  users = [
-    {
-      name: 'Lia Lugo',
-      avatar: 'svg-11',
-      details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
-      'cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta ' +
-      'who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue ' +
-      'castello caerphilly chalk and cheese. Lancashire.',
-      isAdmin: true,
-      isCool: false
-    }
-  ]
-  selectedUser = this.users[0];
-
-  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, private dialog: MdDialog) { }
+  constructor() { }
 
   ngOnInit() { }
-
-  openAdminDialog() {
-    this.dialog.open(DialogComponent).afterClosed()
-      .filter(result => !!result)
-      .subscribe(user => {
-        this.users.push(user);
-        this.selectedUser = user;
-      });
-  }
 }
--- TpMaterial/src/app/pages/overview/overview.component.ts
+++ TpMaterial/src/app/pages/overview/overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MdIconRegistry, MdDialog } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { DialogComponent } from '../../dialog/dialog.component';
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  users = [
+    {
+      name: 'Lia Lugo',
+      avatar: 'svg-11',
+      details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
+      'cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta ' +
+      'who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue ' +
+      'castello caerphilly chalk and cheese. Lancashire.',
+      isAdmin: true,
+      isCool: false
+    }
+  ]
+  selectedUser = this.users[0];
+
+  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, private dialog: MdDialog) { }
 
   ngOnInit() {
   }
 
+  openAdminDialog() {
+    this.dialog.open(DialogComponent).afterClosed()
+      .filter(result => !!result)
+      .subscribe(user => {
+        this.users.push(user);
+        this.selectedUser = user;
+      });
+  }
 }
