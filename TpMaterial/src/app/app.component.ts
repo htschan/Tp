6cec04cce713e617d9f2779d6262bc16c15e5f53
Hans Tschan <Hans.Tschan@gmail.com>
2017-08-20 17:16:05 +0200

    fixed missing symbols

--- TpMaterial/src/app/app.component.ts
+++ TpMaterial/src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import { MdIconRegistry, MdDialog } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 import 'rxjs/add/operator/filter';
-
-import { DialogComponent } from './dialog/dialog.component';
-
 
 @Component({
   selector: 'app-root',
@@ -14,30 +9,7 @@ import { DialogComponent } from './dialog/dialog.component';
 })
 export class AppComponent {
   title = 'app';
-  isDarkTheme = false;
-  users = [
-    {
-      name: 'Lia Lugo',
-      avatar: 'svg-11',
-      details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
-      'cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta ' +
-      'who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue ' +
-      'castello caerphilly chalk and cheese. Lancashire.',
-      isAdmin: true,
-      isCool: false
-    }
-  ]
-  selectedUser = this.users[0];
 
-  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, private dialog: MdDialog) {
-  }
-
-  openAdminDialog() {
-    this.dialog.open(DialogComponent).afterClosed()
-      .filter(result => !!result)
-      .subscribe(user => {
-        this.users.push(user);
-        this.selectedUser = user;
-      });
+  constructor() {
   }
 }
