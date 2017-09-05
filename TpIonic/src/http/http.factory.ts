764d7b5adee4364d329cd119608fd04f9a367788
Hans Tschan <Hans.Tschan@gmail.com>
2017-09-05 19:36:37 +0200

    fixing CORS issues

--- TpIonic/src/http/http.factory.ts
+++ TpIonic/src/http/http.factory.ts
@@ -2,6 +2,6 @@ import { Http, XHRBackend, RequestOptions } from '@angular/http';
 import { InterceptedHttp } from './http.interceptor';
 import { Storage } from '@ionic/storage';
 
-export function httpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, baseurl: any, storage: Storage): Http {
+export function httpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, baseurl: string, storage: Storage): Http {
     return new InterceptedHttp(xhrBackend, requestOptions, baseurl, storage);
 }
