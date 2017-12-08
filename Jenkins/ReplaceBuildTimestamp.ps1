ab26002d310c62aa1972ee32767cf241f5ee45e9
Hans Tschan <Hans.Tschan@gmail.com>
2017-12-08 19:15:52 +0100

    build maintenance

--- Jenkins/ReplaceBuildTimestamp.ps1
+++ Jenkins/ReplaceBuildTimestamp.ps1
@@ -6,4 +6,4 @@ Param(
     [Parameter(Mandatory=$true)]
     [string]$BuildNumber
 )
-(Get-Content $Workspace/TpMaterial/src/timepuncher-client-config.ts) -replace "<buildtimestamp>", "$BuildTimestamp build $BuildNumber" | Set-Content $Workspace/TpMaterial/src/timepuncher-client-config.ts
+(Get-Content $Workspace/TpMaterial/src/app/timepuncher-client-config.ts) -replace "<buildtimestamp>", "$BuildTimestamp build $BuildNumber" | Set-Content $Workspace/TpMaterial/src/app/timepuncher-client-config.ts
