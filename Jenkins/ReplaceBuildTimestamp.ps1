d98c421408438fabb63d508dddd36478432b7714
Hans Tschan <Hans.Tschan@gmail.com>
2017-12-08 19:12:03 +0100

    build maintenance

--- Jenkins/ReplaceBuildTimestamp.ps1
+++ Jenkins/ReplaceBuildTimestamp.ps1
@@ -6,4 +6,4 @@ Param(
     [Parameter(Mandatory=$true)]
     [string]$BuildNumber
 )
-(Get-Content $Workspace/CosmiIonic/src/cosmi-client-config.ts) -replace "<buildtimestamp>", "$BuildTimestamp build $BuildNumber" | Set-Content $Workspace/CosmiIonic/src/cosmi-client-config.ts
+(Get-Content $Workspace/TpMaterial/src/timepuncher-client-config.ts) -replace "<buildtimestamp>", "$BuildTimestamp build $BuildNumber" | Set-Content $Workspace/TpMaterial/src/timepuncher-client-config.ts
